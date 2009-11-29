da3366d644c6884d4551ad25e7149c32ff7b3101
Joel Lehtonen <joell@Joel-Lehtonen-MacBook.local>
2009-11-29 14:19:32 +0200

    Solves now nonograms with one iteration.

---
+++ Silu.hs
@@ -0,0 +1,25 @@
+module Silu where
+
+import Data.List
+import Permutations
+import LineTools
+
+--solve :: ([[Int]],[[Int]]) -> Grid
+solve (rawX,rawY) = iterateV linesY initgrid 
+  where linesX = toLineInfo rawX rawY
+        linesY = toLineInfo rawY rawX
+        initgrid = replicate (length rawX) $ replicate (length rawY) Any  
+
+iterateH :: [LineInfo] -> Grid -> Grid
+iterateH infos grid = zipWith matchIterate infos grid
+
+iterateV :: [LineInfo] -> Grid -> Grid
+iterateV infos grid = transpose $ iterateH infos $ transpose grid
+        
+toLineInfo :: [[Int]] -> [[Int]] -> [LineInfo]
+toLineInfo this other = map mapper this 
+  where mapper x = LineInfo (length other) x
+        
+matchIterate :: LineInfo -> Line -> Line
+matchIterate info previous = lineAnd $ filter (lineOk previous) $
+                             map blackWhiter $ iters info
